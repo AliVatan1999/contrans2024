f657aeab0877eb4bcf3e98baf8c81b477c8ae3b3
AliVatan1999 <wbz5tq@virginia.edu>
2024-11-28 23:44:34 -0500

    Updates

--- contrans.py
+++ contrans.py
@@ -11,6 +11,27 @@ class contrans:
         self.mypassword = os.getenv('mypassword')
         self.congresskey = os.getenv('congresskey')
         self.newskey = os.getenv('newskey')
+        self.us_state_to_abbrev = {
+            "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR",
+            "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE",
+            "Florida": "FL", "Georgia": "GA", "Hawaii": "HI",
+            "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA",
+            "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA",
+            "Maine": "ME", "Maryland": "MD", "Massachusetts": "MA",
+            "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS",
+            "Missouri": "MO", "Montana": "MT", "Nebraska": "NE",
+            "Nevada": "NV", "New Hampshire": "NH", "New Jersey": "NJ",
+            "New Mexico": "NM", "New York": "NY", "North Carolina": "NC",
+            "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK",
+            "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI",
+            "South Carolina": "SC", "South Dakota": "SD", "Tennessee": "TN",
+            "Texas": "TX", "Utah": "UT", "Vermont": "VT",
+            "Virginia": "VA", "Washington": "WA", "West Virginia": "WV",
+            "Wisconsin": "WI", "Wyoming": "WY", "District of Columbia": "DC",
+            "American Samoa": "AS", "Guam": "GU", "Northern Mariana Islands": "MP",
+            "Puerto Rico": "PR", "United States Minor Outlying Islands": "UM",
+            "Virgin Islands": "VI"
+        }
 
     def get_votes(self):
         # Load votes data from URL
@@ -30,25 +51,20 @@ class contrans:
         useragent = json.loads(r.text)['user-agent']
         return useragent
 
-    def make_headers(self, 
-                     email= 'wbz5tq@virginia.edu'):
-        useragent=self.get_useragent()
+    def make_headers(self, email='wbz5tq@virginia.edu'):
+        useragent = self.get_useragent()
         headers = {
             'User-Agent': useragent,
             'From': email
         }
         return headers
 
-
-
-
-
-    def get_bioguideIDs(self):
+    def get_bioguideIDs(self, congress=118):
         params = {'api_key': self.congresskey,
-                  'limit' : 1}
+                  'limit': 1}
         headers = self.make_headers()
         root = 'https://api.congress.gov/v3'
-        endpoint = '/member'
+        endpoint = f'/member/congress/{congress}'
         r = requests.get(root + endpoint, params=params, headers=headers)
         totalrecords = r.json()['pagination']['count']
 
@@ -57,7 +73,7 @@ class contrans:
         bio_df = pd.DataFrame()
 
         while j < totalrecords:
-            params['offset']= j
+            params['offset'] = j
             r = requests.get(root + endpoint, params=params, headers=headers)
             records = pd.json_normalize(r.json()['members'])
             bio_df = pd.concat([bio_df, records])
@@ -65,9 +81,7 @@ class contrans:
 
         return bio_df
 
-
     def get_bioguide(self, name, state=None, district=None):
-
         members = self.get_bioguideIDs()
 
         members['name'] = members['name'].str.lower().str.strip()
@@ -85,9 +99,8 @@ class contrans:
         return members.reset_index(drop=True)
 
     def get_sponsoredlegislation(self, bioguideid):
-
         params = {'api_key': self.congresskey,
-                  'limit' : 1}
+                  'limit': 1}
         headers = self.make_headers()
         root = 'https://api.congress.gov/v3'
         endpoint = f'/member/{bioguideid}/sponsored-legislation'
@@ -96,10 +109,10 @@ class contrans:
 
         params['limit'] = 250
         j = 0
-        bills_list= []
+        bills_list = []
 
         while j < totalrecords:
-            params['offset']= j
+            params['offset'] = j
             r = requests.get(root + endpoint, params=params, headers=headers)
             records = r.json()['sponsoredLegislation']
             bills_list = bills_list + records
@@ -108,20 +121,54 @@ class contrans:
         return bills_list
 
     def get_billdata(self, billurl):
-         r = requests.get(billurl, params={'api_key': self.congresskey})
-         bill_json = json.loads(r.text)
-         texturl = bill_json['bill']['textVersions']['url']
-         r = requests.get(texturl, params={'api_key': self.congresskey})
-         toscrape = json.loads(r.text)['textVersions'][0]['formats'][0]['url']
-         r = requests.get(toscrape)
-         mysoup = BeautifulSoup(r.text, 'html.parser')
-         billtext = mysoup.text
-         bill_json['bill_text'] = billtext
-         return bill_json
+        r = requests.get(billurl, params={'api_key': self.congresskey})
+        bill_json = json.loads(r.text)
+        texturl = bill_json['bill']['textVersions']['url']
+        r = requests.get(texturl, params={'api_key': self.congresskey})
+        toscrape = json.loads(r.text)['textVersions'][0]['formats'][0]['url']
+        r = requests.get(toscrape)
+        mysoup = BeautifulSoup(r.text, 'html.parser')
+        billtext = mysoup.text
+        bill_json['bill_text'] = billtext
+        return bill_json
 
+    def make_cand_table(self):
+        members = self.get_bioguideIDs()
+        replace_map = {'Republican': 'R', 'Democratic': 'D', 'Independent': 'I'}
+        members['partyletter'] = members['partyName'].replace(replace_map)
+        members['state'] = members['state'].replace(self.us_state_to_abbrev)
+        members['district'] = members['district'].fillna(0)
+        members['district'] = members['district'].astype('int').astype('str')
+        members['district'] = ['0' + x if len(x) == 1 else x for x in members['district']]
+        members['district'] = [x.replace('00', 'S') for x in members['district']]
+        members['DistIDRunFor'] = members['state'] + members['district']
+        members['lastname'] = [x.split(',')[0] for x in members['name']]
+        members['firstname'] = [x.split(',')[1] for x in members['name']]
+        members['name2'] = [y.strip() + ' (' + z.strip() + ')'
+                            for y, z in zip(members['lastname'], members['partyletter'])]
 
+        cands = pd.read_csv('cands22.txt', quotechar="|", header=None)
+        cands.columns = ['Cycle', 'FECCandID', 'CID', 'FirstLastP',
+                         'Party', 'DistIDRunFor', 'DistIDCurr',
+                         'CurrCand', 'CycleCand', 'CRPICO', 'RecipCode', 'NoPacs']
+        cands['DistIDRunFor'] = [x.replace('S0', 'S') for x in cands['DistIDRunFor']]
+        cands['DistIDRunFor'] = [x.replace('S1', 'S') for x in cands['DistIDRunFor']]
+        cands['DistIDRunFor'] = [x.replace('S2', 'S') for x in cands['DistIDRunFor']]
+        cands['name2'] = [' '.join(x.split(' ')[-2:]) for x in cands['FirstLastP']]
+        cands = cands[['CID', 'name2', 'DistIDRunFor']].drop_duplicates(subset=['name2', 'DistIDRunFor'])
+        crosswalk = pd.merge(members, cands,
+                             left_on=['name2', 'DistIDRunFor'],
+                             right_on=['name2', 'DistIDRunFor'],
+                             how='left')
+        return crosswalk
 
-
-
-
-
+    def terms_df(self, members):
+        termsDF = pd.DataFrame()
+        for index, row in members.iterrows():
+            bioguide_id = row['bioguideId']
+            terms = row['terms.item']
+            df = pd.DataFrame.from_records(terms)
+            df['bioguideId'] = bioguide_id
+            termsDF = pd.concat([termsDF, df])
+        members = members.drop('terms.item', axis=1)
+        return termsDF, members
